ab5bb3443523bf0a237926f6545337cb72a0d56c
Vibhaj Rajan <vibhaj8@gmail.com>
2016-07-05 18:57:21 +0530

    removed unwanted

--- src/test/java/com/browserstack/LocalTest.java
+++ src/test/java/com/browserstack/LocalTest.java
@@ -3,14 +3,6 @@ package com.browserstack;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Platform;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.remote.Augmenter;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class LocalTest extends BrowserStackJUnitTest {
 
--- src/test/java/com/browserstack/SingleTest.java
+++ src/test/java/com/browserstack/SingleTest.java
@@ -4,13 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Platform;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.remote.Augmenter;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class SingleTest extends BrowserStackJUnitTest {
 
