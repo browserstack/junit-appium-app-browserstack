cec848cc95897d19e2dc0afae98f8ef53dc56b02
Vibhaj Rajan <vibhaj8@gmail.com>
2016-07-06 01:11:04 +0530

    add ide to gitignore

--- src/test/java/com/browserstack/BrowserStackJUnitTest.java
+++ src/test/java/com/browserstack/BrowserStackJUnitTest.java
@@ -22,7 +22,6 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameter;
 import org.junit.runners.Parameterized.Parameters;
 
