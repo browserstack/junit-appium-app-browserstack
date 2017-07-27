f166509a7c8f92b033544cdb39893be2ce9ff621
Harish Ved <ved.harish3@gmail.com>
2017-07-27 11:27:06 +0530

    AppAutomate: new tests to work for app_testing

--- src/test/java/com/browserstack/SingleTest.java
+++ src/test/java/com/browserstack/SingleTest.java
@@ -3,19 +3,26 @@ package com.browserstack;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class SingleTest extends BrowserStackJUnitTest {
 
   @Test
   public void test() throws Exception {
-    driver.get("https://www.google.com/ncr");
-    WebElement element = driver.findElement(By.name("q"));
-    element.sendKeys("BrowserStack");
-    element.submit();
+    WebElement searchElement = new WebDriverWait(driver, 30).until(
+        ExpectedConditions.elementToBeClickable(By.id("Search Wikipedia")));
+    searchElement.click();
+    WebElement insertTextElement = new WebDriverWait(driver, 30).until(
+        ExpectedConditions.elementToBeClickable(By.id("org.wikipedia.alpha:id/search_src_text")));
+    insertTextElement.sendKeys("BrowserStack");
     Thread.sleep(5000);
 
-    assertEquals("BrowserStack - Google Search", driver.getTitle());
+    List<WebElement> allProductsName = driver.findElements(By.className("android.widget.TextView"));
+    assertTrue(allProductsName.size() > 0);
   }
 }
