a68795fb2eab630a33e1ebd8a3050e95f1d953eb
Kamalpreet Kaur <kmlkaur73@gmail.com>
2023-01-23 20:19:06 +0530

    add: sdk samples for JUnit 4, 5

--- ios/junit5-examples/src/test/java/com/browserstack/FirstTest.java
+++ ios/junit5-examples/src/test/java/com/browserstack/FirstTest.java
@@ -1,8 +1,7 @@
 package com.browserstack;
 
 import io.appium.java_client.AppiumBy;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.WebElement;
@@ -15,12 +14,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FirstTest extends BrowserStackJUnitTest{
 
-  @ParameterizedTest
-  @MethodSource("devices")
-  void testCalcOne(int taskId) throws IOException, InterruptedException {
-
-    createConnection(taskId);
-
+  @Test
+  void testCalcOne() throws IOException, InterruptedException {
     WebElement textButton = (WebElement) new WebDriverWait(driver, Duration.ofSeconds(30)).until(
         ExpectedConditions.elementToBeClickable(AppiumBy.accessibilityId("Text Button")));
     textButton.click();
